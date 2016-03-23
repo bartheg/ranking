20c6864330261a53b1644920d4be1333434f34ed
Bartlomiej <b.sielski@yandex.com>
2016-03-23 19:47:08 +0100

    Make user has many profiles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :timeoutable and :omniauthable
+  has_many :profiles
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable, :lockable

c4c496c6a2496573bb223984c9f9412bd3dbbc07
Bartlomiej <b.sielski@yandex.com>
2016-03-23 22:47:46 +0100

    Make validations to Profile

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,7 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+
+  COLOR_REGEX = /\A#([a-f]|[A-F]|[0-9]){3}(([a-f]|[A-F]|[0-9]){3})?\z/
+  validates :user_id, presence: true
+  validates :color, format: COLOR_REGEX, allow_nil: true
 end
--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'validations' do
+    subject { Profile.new user_id: 1, description: "Some description", color: '#eeddaa'}
+
+    it 'is valid when every field is OK' do
+      expect(subject).to be_valid
+    end
+
+    it 'is invalid without user_id' do
+      subject.user_id = nil
+      expect(subject).to be_invalid
+    end
+
+    it 'is valid without description' do
+      subject.description = nil
+      expect(subject).to be_valid
+    end
+
+    it 'is valid without color' do
+      subject.color = nil
+      expect(subject).to be_valid
+    end
+
+    it 'is valid when in html hex color format' do
+      subject.color = '#eEdDaA'
+      expect(subject).to be_valid
+    end
+
+    it 'is valid when in short html hex color format' do
+      subject.color = '#fff'
+      expect(subject).to be_valid
+    end
+
+    it 'is invalid when not in html hex color format' do
+      subject.color = 'eEdDaA'
+      expect(subject).to be_invalid
+    end
+
+  end
+
 end

9e0f48375bceb5f18b6d316e3d6194997f91fb64
Bartlomiej <b.sielski@yandex.com>
2016-04-02 21:50:08 +0200

    Validations for ProfileNameEdit

--- app/models/profile_name_edit.rb
+++ app/models/profile_name_edit.rb
@@ -1,3 +1,12 @@
 class ProfileNameEdit < ActiveRecord::Base
   belongs_to :profile
+
+  NAME_REGEX = /\A(\w|\.|-)+\z/
+  MUST_HAVE_LETTER_REGEX = /[a-zA-Z]+/
+  validates :profile_id, presence: true
+  validates :name, presence: true
+  validates :name, format: NAME_REGEX
+  validates :name, format: MUST_HAVE_LETTER_REGEX
+  validates :name, length: { maximum: 24, minimum: 3 }
+
 end
--- spec/models/profile_name_edit_spec.rb
+++ spec/models/profile_name_edit_spec.rb
@@ -1,5 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe ProfileNameEdit, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'validations' do
+    subject { ProfileNameEdit.new profile_id: 1, name: "Goist-Han"}
+
+    it 'is valid when every field is OK' do
+      expect(subject).to be_valid
+    end
+
+    it 'is invalid without profile_id' do
+      subject.profile_id = nil
+      expect(subject).to be_invalid
+    end
+
+    it 'is invalid without name' do
+      subject.name = nil
+      expect(subject).to be_invalid
+    end
+
+    it 'is valid when name has only basic letters, numbers "-", "." and "_"' do
+      subject.name ='ABJxyz0189-._'
+      expect(subject).to be_valid
+    end
+
+    it 'is invalid when name has url unfriendly characters' do
+      subject.name =';s[=_ #]23wesdc'
+      expect(subject).to be_invalid
+    end
+
+    it 'is invalid when name has more than 24 characters' do
+      subject.name ='a' * 25
+      expect(subject).to be_invalid
+    end
+
+    it 'is invalid when name has less than 3 characters' do
+      subject.name ='a' * 2
+      expect(subject).to be_invalid
+    end
+
+    it 'is invalid when name has not any letter' do
+      subject.name ='12_'
+      expect(subject).to be_invalid
+    end
+
+
+  end
+
 end

f8ba2ff23ea437440faa7fd4ee6a10935e42330f
Bartlomiej <b.sielski@yandex.com>
2016-04-03 14:09:32 +0200

    Language validations

--- spec/models/language_spec.rb
+++ spec/models/language_spec.rb
@@ -1,5 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe Language, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'validations' do
+    subject { Language.new iso_639_1: "et", english_name: "Estonian"}
+
+    it 'is valid when every field is OK' do
+      expect(subject).to be_valid
+    end
+
+    it 'is invalid without iso_639_1' do
+      subject.iso_639_1 = nil
+      expect(subject).to be_invalid
+    end
+
+    it 'is invalid without english_name' do
+      subject.english_name = nil
+      expect(subject).to be_invalid
+    end
+
+    it 'is invalid when iso_639_1 is not a two lower case letters' do
+      subject.iso_639_1 = 'Sa'
+      expect(subject).to be_invalid
+    end
+
+    it 'is invalid when iso_639_1 is not a two lower case letters' do
+      subject.iso_639_1 = 'saa'
+      expect(subject).to be_invalid
+    end
+
+    it 'is invalid when the first letter of english_name is not a upper case' do
+      subject.english_name = 'english'
+      expect(subject).to be_invalid
+    end
+
+
+    # it 'is valid without color' do
+    #   subject.color = nil
+    #   expect(subject).to be_valid
+    # end
+    #
+    # it 'is valid when in html hex color format' do
+    #   subject.color = '#eEdDaA'
+    #   expect(subject).to be_valid
+    # end
+    #
+    #
+    # it 'is invalid when not in html hex color format' do
+    #   subject.color = 'eEdDaA'
+    #   expect(subject).to be_invalid
+    # end
+
+  end
+
 end

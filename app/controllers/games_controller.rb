c56cc766ba8838b5de637998560c88f6c8b8aa4a
Bartlomiej <b.sielski@yandex.com>
2016-05-02 22:37:19 +0200

    Add basic game show view

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,17 @@
 class GamesController < ApplicationController
+  before_action :set_game, only: [:show]
 
   def index
     @games = Game.all
+  end
+
+  def show
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_game
+    @game = Game.find(params[:id])
   end
 
 end

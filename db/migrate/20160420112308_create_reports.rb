054365f758a6d7ab2bae58beddab8ee62c04fe84
Bartlomiej <b.sielski@yandex.com>
2016-05-05 21:22:30 +0200

    Fix create reports migration

--- db/migrate/20160420112308_create_reports.rb
+++ db/migrate/20160420112308_create_reports.rb
@@ -2,7 +2,7 @@ class CreateReports < ActiveRecord::Migration
   def change
     create_table :reports do |t|
       t.references :scenario, index: true, foreign_key: true
-      t.references :reporter, index: true, foreign_key: true
+      t.integer :reporter_id, index: true, foreign_key: true
       t.references :opponent, index: true, foreign_key: true
       t.references :reporters_faction, index: true, foreign_key: true
       t.references :opponents_faction, index: true, foreign_key: true

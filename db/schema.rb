94aadbb640bbbc77d769dc83b5bf059f59a996fa
Bartlomiej <b.sielski@yandex.com>
2016-05-05 22:23:46 +0200

    Mess with migrations again

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160504152748) do
+ActiveRecord::Schema.define(version: 20160504133911) do
 
   create_table "confirmations", force: :cascade do |t|
     t.integer  "report_id"

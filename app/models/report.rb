45cb338913f31c725d114342c816a2a11b9098db
Bartlomiej <b.sielski@yandex.com>
2016-05-27 14:04:34 +0200

    Nothing

--- app/models/report.rb
+++ app/models/report.rb
@@ -8,4 +8,20 @@ class Report < ActiveRecord::Base
   has_many :report_comments
 
   attr_accessor :confirmers_name, :result_description
+
+
+  # COLOR_REGEX = /\A#([a-f]|[A-F]|[0-9]){3}(([a-f]|[A-F]|[0-9]){3})?\z/
+  # validates :user_id, presence: true
+  # validates :color, format: COLOR_REGEX, allow_nil: true
+  #
+  #
+  # NAME_REGEX = /\A(\w|\.|-)+\z/
+  # MUST_HAVE_LETTER_REGEX = /[a-zA-Z]+/
+  # # validates :profile_id, presence: true
+  # validates :name, presence: true
+  # validates :name, format: NAME_REGEX
+  # validates :name, format: MUST_HAVE_LETTER_REGEX
+  # validates :name, length: { maximum: 24, minimum: 3 }
+
+
 end

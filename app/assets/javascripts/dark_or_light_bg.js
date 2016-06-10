945fa7cdc95017370cbc2aec625f2886647f30a1
Bartlomiej <b.sielski@yandex.com>
2016-06-10 16:41:57 +0200

    Half-fix color problem

--- app/assets/javascripts/dark_or_light_bg.js
+++ app/assets/javascripts/dark_or_light_bg.js
@@ -12,6 +12,51 @@
 
     var darkOrLightElements = document.getElementsByClassName(className);
     var hoverElements = document.getElementsByClassName(hoverClassName);
+    var originalColors = [];
+
+    var switchColorFirstTime = function(){
+
+      var red;
+      var green;
+      var blue;
+      var lightness;
+      var bg;
+      var mediumValue = 127; // 255/2
+
+      for (i = 0; i < darkOrLightElements.length; i++) {
+
+        bg = window.getComputedStyle(darkOrLightElements[i]).getPropertyValue('background-color');
+        // bg = darkOrLightElements[i].style.backgroundColor;
+
+        // console.log(darkOrLightElements[i].tagName + " - " + bg);
+        if (bg == "transparent") {
+          var ancestor = darkOrLightElements[i].parentNode;
+          for(;;){
+
+            bg = window.getComputedStyle(ancestor).getPropertyValue('background-color');
+            // bg = darkOrLightElements[i].style.backgroundColor;
+            // console.log(ancestor.tagName + " - " + bg);
+
+            if (bg != "transparent") break;
+            ancestor = ancestor.parentNode;
+          }
+        }
+        console.log(darkOrLightElements[i].tagName + " - " + bg);
+        originalColors[i] = bg;
+
+        red = bg.split("(")[1].split(", ")[0];
+        green = bg.split("(")[1].split(", ")[1];
+        blue = bg.split("(")[1].split(", ")[2].split(")")[0];
+        lightness = (parseInt(red, 10) + parseInt(green, 10) + parseInt(blue, 10)) / 3;
+
+        if (lightness < (mediumValue + toleranceToDark)){
+          darkOrLightElements[i].style.color = lightColor;
+        } else {
+          darkOrLightElements[i].style.color = darkColor;
+        }
+      }
+
+    }
 
     var switchColor = function(){
 
@@ -41,6 +86,34 @@
           }
         }
         console.log(darkOrLightElements[i].tagName + " - " + bg);
+        
+        red = bg.split("(")[1].split(", ")[0];
+        green = bg.split("(")[1].split(", ")[1];
+        blue = bg.split("(")[1].split(", ")[2].split(")")[0];
+        lightness = (parseInt(red, 10) + parseInt(green, 10) + parseInt(blue, 10)) / 3;
+
+        if (lightness < (mediumValue + toleranceToDark)){
+          darkOrLightElements[i].style.color = lightColor;
+        } else {
+          darkOrLightElements[i].style.color = darkColor;
+        }
+      }
+
+    }
+
+
+    var switchColorAfterMouseLeave = function(){
+
+      var red;
+      var green;
+      var blue;
+      var lightness;
+      var bg;
+      var mediumValue = 127; // 255/2
+
+      for (i = 0; i < darkOrLightElements.length; i++) {
+
+        bg = originalColors[i];
 
         red = bg.split("(")[1].split(", ")[0];
         green = bg.split("(")[1].split(", ")[1];
@@ -56,12 +129,13 @@
 
     }
 
+
     if (darkOrLightElements.length > 0) {
-      switchColor();
+      switchColorFirstTime();
       if (hoverElements.length > 0) {
         for (i = 0; i < hoverElements.length; i++) {
           hoverElements[i].addEventListener('mouseenter', switchColor);
-          hoverElements[i].addEventListener('mouseleave', switchColor);
+          hoverElements[i].addEventListener('mouseleave', switchColorAfterMouseLeave);
           // window.addEventListener('mousemove', switchColor);
         }
       }

f0db2c99ec08188db2c564295b64924e900ed7af
Bartlomiej <b.sielski@yandex.com>
2016-06-12 22:26:07 +0200

    Create ladder record for tests

--- spec/models/default_ladder_config_spec.rb
+++ spec/models/default_ladder_config_spec.rb
@@ -4,6 +4,14 @@ RSpec.describe DefaultLadderConfig, type: :model do
 
   describe 'validations' do
 
+    before(:context) do
+      @ladder = Ladder.create!(name: "Super Bowl", description: "Abracadabra.", game_id: 1)
+    end
+
+    after(:context) do
+      Ladder.destroy_all
+    end
+
     subject { DefaultLadderConfig.new(
       default_ranking: 1500,
       loot_factor: 10,
@@ -12,7 +20,7 @@ RSpec.describe DefaultLadderConfig, type: :model do
       draw_factor: 50,
       hours_to_confirm: 48,
       is_default: false,
-      ladder_id: 1)
+      ladder_id: @ladder.id)
     }
 
     it 'is valid when every field is OK' do
@@ -37,13 +45,13 @@ RSpec.describe DefaultLadderConfig, type: :model do
        disproportion_factor: 50,
        draw_factor: 50,
        hours_to_confirm: 48,
-       ladder_id: 1)
+       ladder_id: @ladder.id)
       expect(config).to be_valid
     end
 
     it 'is invalid when is_default is true and ladder_id is not nil' do
       subject.is_default = true
-      subject.ladder_id = 1
+      subject.ladder_id = @ladder.id
       expect(subject).to be_invalid
     end
 

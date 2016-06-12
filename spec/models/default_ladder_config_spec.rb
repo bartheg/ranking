6e4d3c4365be42286e1b39f28e6fa833be36034c
Bartlomiej <b.sielski@yandex.com>
2016-06-12 18:51:18 +0200

    First validation test

--- spec/models/default_ladder_config_spec.rb
+++ spec/models/default_ladder_config_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe DefaultLadderConfig, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'validations' do
+
+    subject { DefaultLadderConfig.new(
+      default_ranking: 1500,
+      loot_factor: 10,
+      loot_constant: 5,
+      disproportion_factor: 50,
+      draw_factor: 50,
+      hours_to_confirm: 48,
+      is_default: false,
+      ladder_id: 1)
+    }
+
+    it 'is valid when every field is OK' do
+      expect(subject).to be_valid
+    end
+
+  end
+
 end

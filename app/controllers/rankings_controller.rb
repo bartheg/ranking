8cc283fd4531cac6ee4f72772ef44c3092bee37c
Bartlomiej <b.sielski@yandex.com>
2016-06-18 20:08:35 +0200

    Create simple reports index

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,4 +1,15 @@
 class RankingsController < ApplicationController
+
   def index
+    if params[:ladder_id]
+      @ladder = Ladder.find(params[:ladder_id].to_i)
+      @header = "Rankings of #{@ladder.name}"
+      @rankings = @ladder.rankings
+
+    else
+      @header = "Rankings"
+      @rankings = Ranking.all
+    end
   end
+
 end

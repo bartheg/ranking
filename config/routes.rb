1a4dda396ad8df10af59df0b60181c38a77750f0
Bartlomiej <b.sielski@yandex.com>
2016-06-20 18:28:03 +0200

    Add a button to create a ladder_config

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,8 @@ Rails.application.routes.draw do
   resources :ladders do
     resources :scenarios, only: [:index, :new, :create]
     resources :rankings, only: [:index]
+    resources :ladder_configs, only: [:show, :new, :create, :edit, :update]
+
   end
 
   resources :reports, only: [:index, :edit, :update]

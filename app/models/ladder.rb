6abdf69ffc58971e0f256bd2859c3ad480ab0d19
Bartlomiej <b.sielski@yandex.com>
2016-07-02 18:01:16 +0200

    Fix has many through association

--- app/models/ladder.rb
+++ app/models/ladder.rb
@@ -2,6 +2,6 @@ class Ladder < ActiveRecord::Base
   belongs_to :game
   has_many :scenarios
   has_many :rankings
-  has_many :reports, through: :scenario
+  has_many :reports, through: :scenarios
   has_one :ladder_config
 end

7aae0adca63ef4a35b099a5aac9c087c01322613
Bartlomiej (@VM) <b.sielski@yandex.com>
2016-09-24 15:17:54 +0200

    Migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160908164346) do
+ActiveRecord::Schema.define(version: 20160924130259) do
 
   create_table "faction_to_scenario_assignments", force: :cascade do |t|
     t.integer  "faction_id"
@@ -174,6 +174,17 @@ ActiveRecord::Schema.define(version: 20160908164346) do
   add_index "reports", ["reporters_faction_id"], name: "index_reports_on_reporters_faction_id"
   add_index "reports", ["scenario_id"], name: "index_reports_on_scenario_id"
 
+  create_table "roles", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "resource_id"
+    t.string   "resource_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "roles", ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
+  add_index "roles", ["name"], name: "index_roles_on_name"
+
   create_table "scenarios", force: :cascade do |t|
     t.string   "full_name"
     t.string   "short_name"
@@ -214,5 +225,12 @@ ActiveRecord::Schema.define(version: 20160908164346) do
   add_index "users", ["profile_id"], name: "index_users_on_profile_id"
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true
+
+  create_table "users_roles", id: false, force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "role_id"
+  end
+
+  add_index "users_roles", ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
 
 end

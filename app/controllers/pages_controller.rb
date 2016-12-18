244067ed7d72f58dca090d6b7e9d909616589463
Bartlomiej (@VM) <b.sielski@yandex.com>
2016-12-18 20:23:27 +0100

    Split max number and real number for last matches and most popular games

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,17 @@
 class PagesController < ApplicationController
 
   def home
-    @reports = Report.all
     add_breadcrumb "Home", :root_path
 
+# for last_matches
+    @max_number_of_last_marches = 4
+    @reports = Report.last(@max_number_of_last_marches)
+    @number_of_last_marches = @reports.count
 # for most_popular_games
-    @number_of_most_popular_games = 2
+    @max_number_of_most_popular_games = 2
     @games = Game.all.sort_by { |game| game.count_reports }
-    @games = @games.first(@number_of_most_popular_games)
-
+    @games = @games.first(@max_number_of_most_popular_games)
+    @number_of_most_popular_games = @games.count
   end
 
   def how_it_works
